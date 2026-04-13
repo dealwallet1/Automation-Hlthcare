56bb19eac148fa94745bce3dd7b9bacb79ab1867
tsssqa001 <tsssqa1@gmail.com>
2026-04-13 09:58:15 +0530

    comments

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,25 +1,4 @@
-# from pages.base_page import BasePage
-# from locators.login_locators import LoginLocators
 
-
-# class LoginPage(BasePage):
-
-#     def login(self, email, password):
-#         self.fill(LoginLocators.EMAIL_INPUT, email)
-#         self.fill(LoginLocators.PASSWORD_INPUT, password)
-#         self.click(LoginLocators.SIGNIN_BUTTON)
-
-#         # small wait for UI update (avoid networkidle issue)
-#         self.page.wait_for_timeout(3000)
-
-#     def get_error(self):
-#         error = self.page.locator(LoginLocators.ERROR_MESSAGE)
-
-#         try:
-#             error.wait_for(state="visible", timeout=5000)
-#             return error.text_content()
-#         except:
-#             return None
 from pages.base_page import BasePage
 from locators.login_locators import LoginLocators
 
